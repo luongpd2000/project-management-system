b742f0cf9a7bd4dd975deb79121b5157fce10f69
luongpd2000 <52768136+luongpd2000@users.noreply.github.com>
2021-08-13 13:01:06 +0700

    create controller

---
+++ back-end/src/main/java/com/projectmanager/controller/AdminController.java
@@ -0,0 +1,8 @@
+package com.projectmanager.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/v1/project_management/admin")
+// api mà chỉ admin dùng được
+public class AdminController {
+}

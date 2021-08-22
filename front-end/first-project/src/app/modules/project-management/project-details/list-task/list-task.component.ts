9915df6ea1c926a5d22b0e7cbeca7739926ed1d5
luongpd2000 <52768136+luongpd2000@users.noreply.github.com>
2021-08-23 01:23:37 +0700

    update role base

--- front-end/first-project/src/app/modules/project-management/project-details/list-task/list-task.component.ts
+++ front-end/first-project/src/app/modules/project-management/project-details/list-task/list-task.component.ts
@@ -17,11 +17,11 @@ import { TodoService } from 'src/app/service/todo.service';
 @Component({
   selector: 'app-list-task',
   templateUrl: './list-task.component.html',
-  styleUrls: ['./list-task.component.css']
+  styleUrls: ['./list-task.component.css'],
 })
 export class ListTaskComponent implements OnInit {
   @Input() currentProjectId: number;
-  @Input() memberList:any[];
+  @Input() memberList: any[];
   @Output() updateDetails = new EventEmitter();
 
   thePageNumber: number = 1;
@@ -44,154 +44,172 @@ export class ListTaskComponent implements OnInit {
 
   selection = new SelectionModel<Task>(true, []);
 
-
-
-  updateParent(){
+  updateParent() {
     this.updateDetails.emit();
     console.log('update parents');
   }
 
-  myUserName!:String;
-  myUserId:number;
-  deleteTask:Task;
-  detailTask: Task= new Task;
+  myUserName!: String;
+  myUserId: number;
+  deleteTask: Task;
+  detailTask: Task = new Task();
   taskHisrotyList: TaskHistory[] = [];
   isAdmin: boolean = false;
+  curUser: User = new User();
+  leaderList = [];
+  taskList: Task[] = [];
+  taskListAll: Task[] = [];
+  formTask!: FormGroup;
+  newTask = new Task();
+  isLeaderOfProject = false;
+  isLeaderOfTask = false;
 
-  leaderList=[];
-  taskList:Task[]=[];
-  taskListAll:Task[]=[];
-  formTask!:FormGroup;
-  newTask=new Task();
-  getLeaderList(){
-    this.memberList.forEach(data=>{
-      if(data.role=="leader") this.leaderList.push(data['user']);
-    })
+  getLeaderList() {
+    this.memberList.forEach((data) => {
+      if (data.role == 'leader') {
+        this.leaderList.push(data['user']);
+        if (data.user.id === this.myUserId) {
+          console.log(data.userId + ' ' + this.myUserId);
+          this.isLeaderOfProject = true;
+        }
+      }
+    });
     console.log('member', this.memberList);
     console.log('leader', this.leaderList);
   }
 
-  constructor(private taskService:TaskService,
-    public getStatus:StatusService,
+  constructor(
+    private taskService: TaskService,
+    public getStatus: StatusService,
     private modalService: NgbModal,
-    private fomatInput:FomatInputService,
-    private jwt:JwtServiceService,
-    private userService:UserService
-    ) { }
+    private fomatInput: FomatInputService,
+    private jwt: JwtServiceService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
     console.log(this.currentProjectId);
     this.getData();
-    this.getLeaderList();
-
     this.myUserName = this.jwt.getUsername();
 
-    if(this.myUserName!=null){
-      if(this.myUserName==="admin"){
+    if (this.myUserName != null) {
+      if (this.myUserName === 'admin') {
         this.isAdmin = true;
-      }else{
+      } else {
         this.isAdmin = false;
       }
-      this.userService.getUser(this.myUserName).subscribe(data=>{
-        this.myUserId=data.id;
-      })
+      this.userService.getUser(this.myUserName).subscribe((data) => {
+        this.curUser = data;
+        this.myUserId = data.id;
+
+        this.getLeaderList();
+      });
     }
     this.getDataAll();
   }
 
-  getData(){
-    this.taskService.getListTaskByProjectIdPageable(this.currentProjectId,this.thePageNumber-1,this.thePageSize).subscribe(
-      data=>{
-      this.taskList = data['content'];
-      console.log(this.taskList);
-      this.dataSource = new MatTableDataSource<Task>(this.taskList);
-      this.thePageNumber = data.pageable.pageNumber + 1;
-      this.thePageSize = data.pageable.pageSize;
-      this.theTotalElements = data.totalElements;
-    },
-    (error) => {
-      console.log(error.error.message);
-    })
+  getData() {
+    this.taskService
+      .getListTaskByProjectIdPageable(
+        this.currentProjectId,
+        this.thePageNumber - 1,
+        this.thePageSize
+      )
+      .subscribe(
+        (data) => {
+          this.taskList = data['content'];
+          console.log(this.taskList);
+          this.dataSource = new MatTableDataSource<Task>(this.taskList);
+          this.thePageNumber = data.pageable.pageNumber + 1;
+          this.thePageSize = data.pageable.pageSize;
+          this.theTotalElements = data.totalElements;
+        },
+        (error) => {
+          console.log(error.error.message);
+        }
+      );
   }
 
-  getDataAll(){
+  getDataAll() {
     this.taskService.getListTaskByProjectId(this.currentProjectId).subscribe(
-      data=>{
-      this.taskListAll = data;
-      console.log(this.taskListAll);
-    },
-    (error) => {
-      console.log(error.error.message);
-    })
+      (data) => {
+        this.taskListAll = data;
+        console.log(this.taskListAll);
+      },
+      (error) => {
+        console.log(error.error.message);
+      }
+    );
   }
 
-
-
-  open(content: any){
+  open(content: any) {
     this.makeForm();
     this.modalService.open(content, {
       centered: true,
-      size: 'lg'
+      size: 'lg',
     });
   }
-  makeForm(){
+  makeForm() {
     this.formTask = new FormGroup({
-      "name":new FormControl(null,[Validators.required]),
-      "des":new FormControl(null,[Validators.required]),
-      "priority":new FormControl('high',[Validators.required]),
-      "startDate":new FormControl(null,[Validators.required]),
-      "endDate":new FormControl(null,[Validators.required]),
-      "status":new FormControl('draft',[Validators.required]),
-      "manager":new FormControl(this.myUserId,[Validators.required]),
-      "taskType":new FormControl("feature", [Validators.required])
-    })
+      name: new FormControl(null, [Validators.required]),
+      des: new FormControl(null, [Validators.required]),
+      priority: new FormControl('high', [Validators.required]),
+      startDate: new FormControl(null, [Validators.required]),
+      endDate: new FormControl(null, [Validators.required]),
+      status: new FormControl('draft', [Validators.required]),
+      manager: new FormControl(this.myUserId, [Validators.required]),
+      taskType: new FormControl('feature', [Validators.required]),
+    });
   }
 
-
-
-  saveNewTask(){
-    if(this.formTask.valid){
-      this.newTask.name=this.formTask.value.name;
-      this.newTask.des=this.formTask.value.des;
-      this.newTask.startDate =this.fomatInput.fomatDate(this.formTask.value.startDate);
-      this.newTask.endDate = this.fomatInput.fomatDate(this.formTask.value.endDate);
-      this.newTask.status=this.formTask.value.status;
-      this.newTask.priority=this.formTask.value.priority;
-      this.newTask.taskManagerId=this.formTask.value.manager;
-      this.newTask.createUser=this.myUserId;
-      this.newTask.taskType=this.formTask.value.taskType;
-      this.newTask.projectId=this.currentProjectId;
+  saveNewTask() {
+    if (this.formTask.valid) {
+      this.newTask.name = this.formTask.value.name;
+      this.newTask.des = this.formTask.value.des;
+      this.newTask.startDate = this.fomatInput.fomatDate(
+        this.formTask.value.startDate
+      );
+      this.newTask.endDate = this.fomatInput.fomatDate(
+        this.formTask.value.endDate
+      );
+      this.newTask.status = this.formTask.value.status;
+      this.newTask.priority = this.formTask.value.priority;
+      this.newTask.taskManagerId = this.formTask.value.manager;
+      this.newTask.createUser = this.myUserId;
+      this.newTask.taskType = this.formTask.value.taskType;
+      this.newTask.projectId = this.currentProjectId;
       console.log('click save!!');
       console.log(JSON.stringify(this.newTask));
-      this.taskService.createTask(this.newTask).subscribe(data=>{
-        console.log('new',data);
+      this.taskService.createTask(this.newTask).subscribe((data) => {
+        console.log('new', data);
         this.modalService.dismissAll();
         this.getData();
-      })
-
-    }else{
-      alert("Input invalid!!!")
+      });
+    } else {
+      alert('Input invalid!!!');
     }
   }
 
-  openConfirmDelete(content: any, task:Task){
-    this.deleteTask=task;
+  openConfirmDelete(content: any, task: Task) {
+    this.isLeaderOfTask = false;
+    if (task.taskManagerId === this.myUserId) {
+      this.isLeaderOfTask = true;
+    }
+    this.deleteTask = task;
     this.modalService.open(content, {
       centered: true,
     });
-
   }
-  onDelete(){
+  onDelete() {
     console.log(this.deleteTask);
-    this.taskService.deleteTask(this.deleteTask).subscribe(data=>{
+    this.taskService.deleteTask(this.deleteTask).subscribe((data) => {
       console.log(data);
       this.getData();
-    })
+    });
     this.modalService.dismissAll();
-
   }
 
-  openHistory(content: any, element){
+  openHistory(content: any, element) {
     this.detailTask = element;
     console.log(this.detailTask);
     this.taskHisrotyList = this.detailTask.taskHistoryList;
@@ -202,46 +220,42 @@ export class ListTaskComponent implements OnInit {
     });
   }
 
-
   updatePageSize(event) {
     this.thePageSize = event.target.value;
-    console.log(this.thePageSize)
+    console.log(this.thePageSize);
     this.thePageNumber = 1;
     this.getData();
-
   }
 
   applyFilter(filterValue: string) {
-    if(filterValue.trim()!=="" || filterValue.trim()!==null){
+    if (filterValue.trim() !== '' || filterValue.trim() !== null) {
       this.dataSource = new MatTableDataSource<Task>(this.taskListAll);
-    }else{
+    } else {
       this.dataSource = new MatTableDataSource<Task>(this.taskList);
     }
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-
   // use for Form
-  get name(){
+  get name() {
     return this.formTask.get('name');
   }
-  get des(){
+  get des() {
     return this.formTask.get('des');
   }
-  get priority(){
-    return this.formTask.get('priority')
+  get priority() {
+    return this.formTask.get('priority');
   }
-  get startDate(){
+  get startDate() {
     return this.formTask.get('startDate');
   }
-  get endDate(){
+  get endDate() {
     return this.formTask.get('endDate');
   }
-  get status(){
+  get status() {
     return this.formTask.get('status');
   }
-  get manager(){
+  get manager() {
     return this.formTask.get('manager');
   }
-
 }
